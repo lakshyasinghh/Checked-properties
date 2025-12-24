437e32a25d5d23188f89e1824eb8c984b061643f
lakshyasinghh <lakshya.freelancer.org@gmail.com>
2025-12-24 20:21:37 +0530

    Add files via upload

---
+++ checked properties/script.js
@@ -0,0 +1,29 @@
+const mycheckbox = document.getElementById("mycheckbox");
+const visabtn = document.getElementById("visabtn")
+const rupaybtn = document.getElementById("rupaybtn")
+const Upi = document.getElementById("Upi")
+const mysubmit = document.getElementById("mysubmit")
+const  subresult = document.getElementById("subresult")
+const paymentresult = document.getElementById("paymentresult")
+
+mysubmit.onclick = function(){
+
+    if(mycheckbox.checked){
+        subresult.textContent = `YOU ARE SUBSCRIBED!`;
+    }
+    else{
+        subresult.textContent = `YOU ARE NOT SUBSCRIBED!!`;
+    }
+    if(visabtn.checked){
+     paymentresult.textContent = `you are paying with visa`;
+    }
+    else if(rupaybtn.checked){
+     paymentresult.textContent = `you are paying with rupay`;
+    }
+    else if(Upi.checked){
+        paymentresult.textContent = `you are paying with Upi`;
+    }
+    else{
+        paymentresult.textContent = `you haven't selected a payment result,are you broke ?????`
+    }
+}
